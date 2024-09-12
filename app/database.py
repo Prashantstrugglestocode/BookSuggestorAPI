584588027fb1f0e5d06e677a9878a29be8d6d82e
Prashant kumar mishra <mishraprashant62@yahoo.in>
2024-09-13 01:20:10 +0530

    added the models for this project in models.py

--- app/database.py
+++ app/database.py
@@ -15,4 +15,15 @@ HOST=os.getenv('HOST')
 
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{HOST}/{DB_TABLE}"
-print(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+Base = declarative_base
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
